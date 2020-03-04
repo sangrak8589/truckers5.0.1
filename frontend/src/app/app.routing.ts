6bcb23b77548f1c060df1b4da36f4a39fea02c34
Sangram <Sandy@35.201.21.198.tigernet.wifi.dyn.clemson.edu>
2020-03-03 21:09:31 -0500

    frontend with angular cli

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,23 @@
+﻿import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home";
+import { DashboardComponent } from "./dashboard";
+import { LoginComponent } from "./login";
+import { RegisterComponent } from "./register";
+import { AuthGuard } from "./_helpers";
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: "**", redirectTo: "dashboard" }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
